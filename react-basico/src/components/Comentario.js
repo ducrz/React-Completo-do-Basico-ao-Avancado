26337d6b39aceded92fb656ffcdcf509f1edb48d
ducrz <ducatolico@gmail.com>
2021-10-30 13:31:27 -0300

    Fim Seção 04: Conceitos Básicos e Síntaxe. Instalando Pacotes.

--- react-basico/src/components/Comentario.js
+++ react-basico/src/components/Comentario.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { formatRelative } from 'date-fns';
+import { ptBR } from 'date-fns/locale'
 import './Comentario.css';
 import imagemUsuario from './user.png';
 
@@ -11,7 +13,7 @@ const Comentario = props => {
             <h2 class="nome">{props.nome}</h2>
             <p class="email">{props.email}</p>
             <p class="mensagem">{props.children}</p>
-            <p class="data">{props.data.toString()}</p>
+            <p class="data">{formatRelative(props.data, new Date(), {locale: ptBR})}</p>
             <button onClick={props.onRemove}>&times;</button>
         </div>
         </div>       
