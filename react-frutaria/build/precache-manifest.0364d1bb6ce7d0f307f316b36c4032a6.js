71c89105ed931f5df26b72538b11a9dac9cfc883
ducrz <ducatolico@gmail.com>
2021-11-01 14:57:07 -0300

    Ajustes

---
+++ react-frutaria/build/precache-manifest.0364d1bb6ce7d0f307f316b36c4032a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4076df6c63271c5c9f2b22d3f6514053",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ee7000c8aef5f883cab",
+    "url": "/static/css/main.ca85a575.chunk.css"
+  },
+  {
+    "revision": "c09b27350b817e933b9f",
+    "url": "/static/js/2.727d54b9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.727d54b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ee7000c8aef5f883cab",
+    "url": "/static/js/main.23e503be.chunk.js"
+  },
+  {
+    "revision": "0481832ad9d84c14443f",
+    "url": "/static/js/runtime-main.43ed6a93.js"
+  }
+]);
