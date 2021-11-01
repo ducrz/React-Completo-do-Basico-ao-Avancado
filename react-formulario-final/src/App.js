e3b6ca0a5fd937175a7e2521af1b8309af1b5c8a
ducrz <ducatolico@gmail.com>
2021-11-01 14:29:41 -0300

    Fim Seção 09: Validações de Formulário.

---
+++ react-formulario-final/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import AdicionaCliente from './components/AdicionaCliente';
+
+function App() {
+  return (
+    <div className="container">
+      <AdicionaCliente />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-formulario-final/src/components/AdicionaCliente.jsx
@@ -0,0 +1,68 @@
+import { Formik, useField } from 'formik';
+import React from 'react';
+import * as yup from 'yup';
+
+const Campo = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+  return (
+    <div className="form-group">
+      <label htmlFor={props.id}>{label}</label>
+      <input
+        {...field}
+        {...props}
+        className={meta.error && meta.touched ? 'is-invalid' : ''}
+      />
+      {meta.error && meta.touched ? (
+        <div className="invalid-feedback">{meta.error}</div>
+      ) : null}
+    </div>
+  );
+};
+
+const AdicionaCliente = () => {
+  const esquema = yup.object({
+    nome: yup
+      .string()
+      .required('O nome é obrigatório')
+      .min(10, 'O nome deve ter no mínimo 10 caracteres')
+      .max(30, 'O nome deve ter no máximo 30 caracteres'),
+    email: yup
+      .string()
+      .required('O email é obrigatório')
+      .email('O email é inválido'),
+    nascimento: yup
+      .date()
+      .required('A data de nascimento é obrigatória')
+      .max(new Date(), 'Você não pode ter nascido no futuro...'),
+  });
+
+  return (
+    <>
+      <h1>Cadastro de Clientes</h1>
+
+      <Formik
+        initialValues={{ nome: '', email: '', nascimento: '' }}
+        validationSchema={esquema}
+        onSubmit={(values) => {
+          alert(JSON.stringify(values));
+        }}
+      >
+        {(props) => (
+          <form onSubmit={props.handleSubmit} noValidate>
+            <Campo type="text" id="nome" name="nome" label="Nome" />
+            <Campo type="email" id="email" name="email" label="Email" />
+            <Campo
+              type="date"
+              id="nascimento"
+              name="nascimento"
+              label="Data de Nascimento"
+            />
+            <button type="submit">Adicionar</button>
+          </form>
+        )}
+      </Formik>
+    </>
+  );
+};
+
+export default AdicionaCliente;
